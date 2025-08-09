20e4ced1015a4a4e8082ca620dd6c3d5dec89845
Leandro Ademar <leandro.ademars@gmail.com>
2025-08-09 19:21:25 -0300

    chore: remover arquivos de configuração de ambiente (.env.example) e docker-compose.yml, limpando o repositório de arquivos desnecessários

--- apps/web/app/(all)/[workspaceSlug]/(projects)/header.tsx
+++ apps/web/app/(all)/[workspaceSlug]/(projects)/header.tsx
@@ -1,14 +1,10 @@
 "use client";
 
 import { observer } from "mobx-react";
-import Image from "next/image";
 import { useTheme } from "next-themes";
 import { Home, Shapes } from "lucide-react";
 // images
-import githubBlackImage from "/public/logos/github-black.png";
-import githubWhiteImage from "/public/logos/github-white.png";
 // ui
-import { GITHUB_REDIRECTED_TRACKER_EVENT, HEADER_GITHUB_ICON } from "@plane/constants";
 import { useTranslation } from "@plane/i18n";
 import { Breadcrumbs, Button, Header } from "@plane/ui";
 // components
@@ -48,31 +44,6 @@ export const WorkspaceDashboardHeader = observer(() => {
             <Shapes size={16} />
             <div className="hidden text-xs font-medium sm:hidden md:block">{t("home.manage_widgets")}</div>
           </Button>
-          <a
-            onClick={() =>
-              captureElementAndEvent({
-                element: {
-                  elementName: HEADER_GITHUB_ICON,
-                },
-                event: {
-                  eventName: GITHUB_REDIRECTED_TRACKER_EVENT,
-                  state: "SUCCESS",
-                },
-              })
-            }
-            className="flex flex-shrink-0 items-center gap-1.5 rounded bg-custom-background-80 px-3 py-1.5"
-            href="https://plane.nwerp.ai"
-            target=""
-            rel="noopener noreferrer"
-          >
-            <Image
-              src={resolvedTheme === "dark" ? githubWhiteImage : githubBlackImage}
-              height={16}
-              width={16}
-              alt="GitHub Logo"
-            />
-            <span className="hidden text-xs font-medium sm:hidden md:block">{t("home.star_us_on_github")}</span>
-          </a>
         </Header.RightItem>
       </Header>
     </>
